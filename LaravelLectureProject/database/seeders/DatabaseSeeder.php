d37d2338bb24c8dc36d1fb04415d2762099152aa
WMatheist <wmatheist@protonmail.com>
2025-11-12 20:19:10 +0900

    Seed admin user and sample Regions/Radios

--- LaravelLectureProject/database/seeders/DatabaseSeeder.php
+++ LaravelLectureProject/database/seeders/DatabaseSeeder.php
@@ -5,14 +5,24 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use App\Models\User;
+use App\Models\Region;
+use App\Models\Radio;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    public function run(): void
     {
         User::updateOrCreate(
             ['email' => 'admin@admin.com'],
             ['name' => 'Admin', 'password' => Hash::make('password'), 'role' => 'admin']
         );
+
+        if (Region::count() === 0) {
+            $regions = collect(['North','South','East','West'])->map(fn($n) => Region::create(['name'=>$n]));
+            foreach ($regions as $reg) {
+                Radio::create(['region_id'=>$reg->id,'name'=>$reg->name.' FM','frequency'=>'101.1']);
+                Radio::create(['region_id'=>$reg->id,'name'=>$reg->name.' AM','frequency'=>'720']);
+            }
+        }
     }
 }
