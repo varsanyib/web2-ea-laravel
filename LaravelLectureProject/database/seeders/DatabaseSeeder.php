51124b4c2c205a52d9cc151d1bee34b160a6f740
WMatheist <wmatheist@protonmail.com>
2025-11-12 19:13:05 +0900

    Seed default admin user

--- LaravelLectureProject/database/seeders/DatabaseSeeder.php
+++ LaravelLectureProject/database/seeders/DatabaseSeeder.php
@@ -2,23 +2,17 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::updateOrCreate(
+            ['email' => 'admin@admin.com'],
+            ['name' => 'Admin', 'password' => Hash::make('password'), 'role' => 'admin']
+        );
     }
 }
